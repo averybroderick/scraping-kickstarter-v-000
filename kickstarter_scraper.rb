ae128fc981c1491fb05f617b5b184f67c8829a88
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-20 14:49:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,6 @@
 require 'nokogiri'
 
 def create_project_hash
-  # write your code here
-  #returns a hash of five project hashes
-  #title points to hash of image link, descr., location, percentage funded
 
   html = File.read('fixtures/kickstarter.html')
 
