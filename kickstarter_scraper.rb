a85a399d2c56b0de0504e8f4c654b89472076b2b
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-20 14:48:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,8 @@
 # require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
   # write your code here
+  #returns a hash of five project hashes
+  #title points to hash of image link, descr., location, percentage funded
 end
